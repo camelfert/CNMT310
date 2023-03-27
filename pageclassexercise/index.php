ae65a2e57c1fc291b2420788e145027c7fe75d18
cam elfert <bcheese12345678@gmail.com>
2023-03-27 11:52:28 -0500

    add head element calls for external JS and CSS

--- pageclassexercise/index.php
+++ pageclassexercise/index.php
@@ -4,11 +4,13 @@ require_once(__DIR__ . "/Page.class.php");
 
 $page = new Page("Page");
 
+$page->addHeadElement("<script src=\"js/script.js\"></script>");
+$page->addHeadElement("<link rel=\"stylesheet\" href=\"css/styles.css\">");
+
 print $page->getTopSection();
 print "<h1>Page created with Page class</h1>" . PHP_EOL;
 print "<p>Paragraph created with Page class</p>" . PHP_EOL;
 print "<h2>Test H2</h2>" . PHP_EOL;
 print "<h3>This is a super fun assignment!</h3>" . PHP_EOL;
-
 print $page->getBottomSection();
 
